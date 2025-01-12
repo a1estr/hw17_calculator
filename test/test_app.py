d6f9ceb133d7d5f1f0bf890e53935ba0b0748fbc
Aliaksei Strakhau <straxov.9942@gmail.com>
2025-01-12 19:36:28 +0300

    Home Work 17

--- test/test_app.py
+++ test/test_app.py
@@ -13,5 +13,29 @@ def test_divide():
     """Тестирует функцию деления."""
     assert divide(10, 2) == 5
     assert divide(9, 3) == 3
-    # with pytest.raises(ValueError):
-    #     divide(5, 0)
+    with pytest.raises(ValueError):
+        divide(5, 0)
+
+
+def test_multiply():
+    """
+    Тестирует функцию умножения
+    """
+    assert multiply(5, 8) == 40
+    assert multiply(8, 5) == 40
+    assert multiply(0, 3) == 0
+    assert multiply(-5, 8) == -40
+    assert multiply(5, -8) == -40
+    assert multiply(-5, -8) == 40
+
+
+def test_subtract():
+    """
+    Тестирует функцию вычитания
+    """
+    assert subtract(10, 7) == 3
+    assert subtract(5, 11) == -6
+    assert subtract(-10, 11) == -21
+    assert subtract(10, -11) == 21
+    assert subtract(-10, -11) == 1
+
